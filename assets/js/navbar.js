88b943983caf767ec339634473a9db0555b70836
Luke Keller <lukeekeller@gmail.com>
2018-04-04 18:47:56 -0400

    WIP Search
    Adds a WIP search bar and User info.
    
    * Add WIP search bar
    * Add user info

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,6 @@
 (function() {
-  var dropdownParents = document.getElementsByClassName("dropdown-parent");
+  var dropdownParents = document.getElementsByClassName("dropdown-parent"),
+      searchButton = document.getElementById("nav-search-button");
 
   document.querySelectorAll('.dropdown-parent').forEach(function(a) {
 
@@ -10,9 +11,20 @@
       // Now do something with my button
   });
 
+  searchButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    showSearchBar(event);
+  });
+
   function showDropdown(a) {
     var dropdown = a.target.nextElementSibling;
 
     dropdown.classList.toggle("active");
-  }
+  };
+
+  function showSearchBar(elem) {
+    var searchParent = document.getElementById("nav-search-container");
+
+    searchParent.classList.add("active");
+  };
 })();
